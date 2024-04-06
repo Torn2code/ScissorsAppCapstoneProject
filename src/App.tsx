8ffe12338557d88a86d982ce5a0b497993d8bbfa
Torn2code <jubriladebiyi@yahoo.com>
2024-04-06 11:10:00 +0100

    Paste Url section bug removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
-import Routes from './Routes';
-import PasteUrl from '../src/components/PasteUrl';
-import Analytics from '../src/Pages/Analytics';
+import React from "react";
+import Routes from "./Routes";
 
 const App: React.FC = () => {
-  const [shortUrls, setShortUrls] = useState<string[]>([]);
-
-  const handleUrlShortened = (shortUrl: string) => {
-    setShortUrls([...shortUrls, shortUrl]);
-  };
-
   return (
     <div className="app">
       <Routes />
-      <PasteUrl onUrlShortened={handleUrlShortened} /> {/* Pass onUrlShortened prop */}
-      <Analytics />
     </div>
   );
 };
